0ec6627fdf160ced3b895c6cdf18b6f23d808f5f
Ben <manglebot@users.noreply.github.com>
2023-11-30 15:32:09 +0000

    fix:add chuck greeting

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import { useState } from 'react';
 import ChuckCard from './components/chuck_card';
 import ChuckInfo from './components/chuck_info';
+import ChuckJokes from './components/chuck_joke';
+
 import Joke from './joke';
 
 function App() {
@@ -30,12 +32,16 @@ function App() {
 		<div className="App">
 
 			<h1>React props and state</h1>
-			<ChuckCard />
+			<ChuckCard chuckGreeting={chuckGreeting}/>
 
 			<h2>Chuck Info: </h2>
-			<ChuckInfo />
+			{/* <ChuckInfo whalesSaved={whalesSaved} roundHouseKicks={roundHouseKicks} /> */}
 
 			<h2>Jokes: </h2>
+			
+			{}
+			
+
 
 		</div>
 	);
--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,16 +1,16 @@
 import ChuckImage from '../assets/chuck_norris.jpg';
 
 interface ChuckCardProps {
-
+	chuckGreeting:string;
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = ({}) => {
+const ChuckCard: React.FC<ChuckCardProps> = ({chuckGreeting}) => {
 
 	return (
 
 		<>
 			{}
-			<h2>{ }</h2>
+			<h2> {chuckGreeting }</h2>
 
 			<img className='img__small' src={ChuckImage} alt='A handsome man' />
 		</>
