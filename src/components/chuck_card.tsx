80c09e301c4d0b25277a29d0940400c21c35bbb8
Ben <manglebot@users.noreply.github.com>
2023-11-30 12:33:20 +0000

    fix:update chuck card to display chuck mugshot

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -8,13 +8,12 @@ interface ChuckCardProps {
 const ChuckCard: React.FC<ChuckCardProps> = ({}) => {
 
 	return (
-		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
-		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
+
 		<>
-			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
+			{}
 			<h2>{ }</h2>
 
-			<img src={'Chuck image here!'} alt='A handsome man' />
+			<img src={ChuckImage} alt='A handsome man' />
 		</>
 	)
 }
