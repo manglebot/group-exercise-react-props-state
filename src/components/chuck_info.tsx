ea5e1a17f0541e8cf3ef4c6ddd49769ef96c1775
Ben <manglebot@users.noreply.github.com>
2023-11-30 16:45:40 +0000

    fix:tweak code to work properly

--- src/components/chuck_info.tsx
+++ src/components/chuck_info.tsx
@@ -1,20 +1,9 @@
-const ChuckInfo = () => {
+interface chuckInfoProp {
+	whalesSaved : number;
+	roundHouseKicks : number;
+}
 
-	// 💡 Note that this component, like the <ChuckCard />, uses an "explicit return"
-	// Compare this to <ChuckJoke/> which just returns the JSX directly like this:
-	// const ChuckJoke = () => <>... some JSX Here...</>;
-
-	// Instead, this component uses curly braces `{}` and a `return` statement
-
-	// This allows us to write code here to perform some logic before we construct 
-	// the JSX we want to return
-
-	// If you don't have any particular need to write TypeScript/JavaScript logic 
-	// in a component, then it's cleaner to use the implicit return style from 
-	// <ChuckJoke/> 
-
-	const whalesSaved = "56"; // 💡 you don't need this to finish this exercise, it's just to demo that JS can go here!
-	const roundHouseKicks = "345";
+const ChuckInfo:React.FC<chuckInfoProp> = ({whalesSaved, roundHouseKicks}) => {
 
 	return (
 		<>
