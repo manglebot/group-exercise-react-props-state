3f5ff2ca15514c305ead45f8946086512e9e1ca7
Ben <manglebot@users.noreply.github.com>
2023-12-06 18:38:20 +0000

    fix:move joke data to external json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,33 @@
-import './App.css';
-import { useState } from 'react';
-import ChuckCard from './components/chuck_card';
-import ChuckInfo from './components/chuck_info';
-import ChuckJokes from './components/chuck_joke';
+import "./App.css";
+import { useState } from "react";
+import ChuckCard from "./components/chuck_card";
+import ChuckInfo from "./components/chuck_info";
+import ChuckJokes from "./components/chuck_joke";
+import jokes from "./data/jokes.json";
 
-import Joke from './joke';
+import Joke from "./joke";
 
 function App() {
+  const [chuckGreeting, setChuckGreeting] = useState<string>(
+    "I am Chuck Norris!!!"
+  );
+  const [whalesSaved, setWhalesSaved] = useState<number>(700);
+  const [roundHouseKicks, setRoundHouseKicks] = useState<number>(300000);
 
-	const [chuckGreeting, setChuckGreeting] = useState<string>("I am Chuck Norris!!!")
-	const [whalesSaved, setWhalesSaved] = useState<number>(700)
-	const [roundHouseKicks, setRoundHouseKicks] = useState<number>(300000)
-	const [jokes, setJokes] = useState<Array<Joke>>([{
-		"id": 1,
-		"joke": "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
-	},
-	{
-		"id": 2,
-		"joke": "Time waits for no man. Unless that man is Chuck Norris.",
-	},
-	{
-		"id": 3,
-		"joke": "The dinosaurs looked at Chuck Norris the wrong way once. You know what happened to them.",
-	},
-	{
-		"id": 4,
-		"joke": "Chuck Norris does not own a stove, oven, or microwave, because revenge is a dish best served cold.",
-	}])
+  return (
+    <div className="App">
+      <h1>React props and state</h1>
+      <ChuckCard chuckGreeting={chuckGreeting} />
 
-	return (
-		<div className="App">
+      <h2>Chuck Info: </h2>
+      <ChuckInfo whalesSaved={whalesSaved} roundHouseKicks={roundHouseKicks} />
 
-			<h1>React props and state</h1>
-			<ChuckCard chuckGreeting={chuckGreeting}/>
-
-			<h2>Chuck Info: </h2>
-			<ChuckInfo whalesSaved={whalesSaved} roundHouseKicks={roundHouseKicks} />
-
-			<h2>Jokes: </h2> 
-			{
-				jokes.map((joke) => {
-					return <ChuckJokes key={joke.id} id={joke.id} joke={joke.joke} />
-				})
-			}
-		</div>
-	);
+      <h2>Jokes: </h2>
+      {jokes.map((joke) => {
+        return <ChuckJokes key={joke.id} id={joke.id} joke={joke.joke} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,8 +1,14 @@
 interface ChuckJokeProps {
-    id: number;
-    joke: string;
+  id: number;
+  joke: string;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = ({id, joke}) => <ul><li className="joke"><strong>{id}:</strong> {joke}</li></ul>
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ id, joke }) => (
+  <ul>
+    <li className="joke">
+      <strong>{id}:</strong> {joke}
+    </li>
+  </ul>
+);
 
 export default ChuckJoke;
